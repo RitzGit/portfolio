197d79abbec660dfa8bda9c4d0b3dd9a3ffa6a44
mwallenfang <marvin@wallenfang.de>
2022-07-31 21:34:41 +0200

    rewrite, scrap vue

--- docs/js/chunk-7460592b.fa269664.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-7460592b"],{"1b4a":function(n,w,o){}}]);
-//# sourceMappingURL=chunk-7460592b.fa269664.js.map
--- docs/js/chunk-774c9b9e.fce400ef.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-774c9b9e"],{fe6b:function(n,w,c){}}]);
-//# sourceMappingURL=chunk-774c9b9e.fce400ef.js.map
